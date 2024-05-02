2493a87668661d8a3577e3ed528b11836f5440e7
Andriizachepilo <zaandrey1@gmail.com>
2024-05-02 19:32:58 +0100

    feat:update security groups for public lb and ec2 instances

--- terraform/modules/Security_Group/main.tf
+++ terraform/modules/Security_Group/main.tf
@@ -1,101 +1,3 @@
-# # Bastion Security Group
-# resource "aws_security_group" "bastion_security_group" {
-#   name        = "bastion-sg"
-#   description = "Security group for bastion host"
-#   vpc_id      = var.vpc_id
-
-#   ingress {
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = [format("%s/%s", data.external.myip.result["internet_ip"], 32)]
-#   }
-# }
-
-# #Private instance Security Group
-# resource "aws_security_group" "private_instance_sg" {
-#   name   = "private-sg"
-#   vpc_id = var.vpc_id
-
-
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-# }
-
-
-# resource "aws_security_group" "public_instances_sg" {
-#   ingress = {
-#     from_port  = 80
-#     to_port    = 443
-#     protocol   = "tcp"
-#     cidr_block = ["0.0.0.0/0"]
-#   }
-
-#   egress = {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
-
-
-
-
-
-# # Public Instances Security Group
-# resource "aws_security_group" "public_security_group" {
-#   name        = "public-sg"
-#   description = "Security group for public EC2 instances "
-#   vpc_id      = var.vpc_id
-
-
-#   ingress {
-#     from_port       = 22
-#     to_port         = 22
-#     protocol        = "tcp"
-#     security_groups = [aws_security_group.bastion_security_group.id]
-#   }
-
-
-#   ingress {
-#     from_port       = 80
-#     to_port         = 443
-#     protocol        = "tcp"
-#     security_groups = [aws_security_group.bastion_security_group.id]
-#   }
-# }
-
-# # Private Instances Security Group
-# resource "aws_security_group" "private_security_group" {
-#   name        = "private-sg"
-#   description = "Security group for private EC2 instance"
-#   vpc_id      = var.vpc_id
-
-
-#   ingress {
-#     from_port       = 22
-#     to_port         = 22
-#     protocol        = "tcp"
-#     security_groups = [aws_security_group.bastion_security_group.id]
-#   }
-
-
-#   ingress {
-#     from_port       = 80
-#     to_port         = 443
-#     protocol        = "tcp"
-#     security_groups = [aws_security_group.bastion_security_group.id]
-#   }
-# }
-
-
-
 data "external" "myip" {
 
   program = ["bash", "${path.module}/script.sh"]
@@ -108,7 +10,7 @@ resource "aws_security_group" "bastion_sg" {
   description = "Security group for bastion host"
   vpc_id      = var.vpc_id
 
-
+#allow ingress only from my ip into the bastion instance
   ingress {
     from_port   = 22
     to_port     = 22
@@ -116,74 +18,68 @@ resource "aws_security_group" "bastion_sg" {
     cidr_blocks = [format("%s/%s", data.external.myip.result["internet_ip"], 32)]
   }
 
-
+  #allow egress to ssh to the instances
   egress {
-    from_port   = 80
-    to_port     = 443
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
 }
 
-# Public Instances Security Group
-resource "aws_security_group" "public_sg" {
-  name        = "public-security-groups"
-  description = "Security group for public EC2 instances"
-  vpc_id      = var.vpc_id
-
-
-  ingress {
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = [aws_security_group.bastion_sg.id]
-  }
-
-  ingress {
-    from_port       = 80
-    to_port         = 443
-    protocol        = "tcp"
-    security_groups = [aws_security_group.bastion_sg.id]
-  }
-
-}
-
-# Private Instances Security Group
-resource "aws_security_group" "private_sg" {
-  name        = "private-security-groups"
-  description = "Security group for private EC2 instance"
-  vpc_id      = var.vpc_id
-
-
-  ingress {
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = [aws_security_group.bastion_sg.id]
-  }
-
-
-  ingress {
-    from_port       = 80
-    to_port         = 443
-    protocol        = "tcp"
-    security_groups = [aws_security_group.bastion_sg.id]
-  }
-
-}
-
-resource "aws_security_group" "load_balancer_sg" {
+resource "aws_security_group" "public_instance_sg" {
   vpc_id = var.vpc_id
 
-  ingress = {
-    from_port  = 80
-    to_port    = 443
+
+  #allow ssh from bastion instance 
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id]
+  }
+
+
+  #allow http and https traffic only from the load balancer 
+  ingress {
+    from_port       = 3000
+    to_port         = 3003
+    protocol        = "tcp"
+    security_groups = [aws_security_group.load_balancer_sg.id]
+  }
+
+  #allow all egress for "git clone " and "docker pull"
+  egress = {
+    from_port  = 0
+    to_port    = 65535
     protocol   = "tcp"
     cidr_block = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_security_group" "private_instance_sg" {
+  vpc_id = var.vpc_id
+
+  #allow ingress only from the internal load balancer
+  ingress {
+    from_port       = 3000
+    to_port         = 3000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.internal_lb_sg.id]
+  }
+
+  #allow ssh from bastion instance
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id]
 
   }
 
+  #allow all egress for "git clone" and "docker pull"
   egress = {
     from_port  = 0
     to_port    = 65535
@@ -192,35 +88,31 @@ resource "aws_security_group" "load_balancer_sg" {
   }
 }
 
-resource "aws_security_group" "public_app_sg" {
+
+resource "aws_security_group" "load_balancer_sg" {
   vpc_id = var.vpc_id
 
-  ingress {
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = [aws_security_group.bastion_sg.id]
+#allow all ingress traffic to the public load balancer
+  ingress = {
+    from_port  = 80
+    to_port    = 443
+    protocol   = "tcp"
+    cidr_block = ["0.0.0.0/0"]
+
   }
 
-  ingress {
-    from_port       = 3000
-    to_port         = 3003
+}
+
+#allow ingress traffic only from the public load balancer
+resource "aws_security_group" "internal_lb_sg" {
+  vpc_id = var.vpc_id
+
+  ingress = {
+    from_port       = 80
+    to_port         = 443
     protocol        = "tcp"
     security_groups = [aws_security_group.load_balancer_sg.id]
   }
 
-  egress = {
-    from_port  = 0
-    to_port    = 55555
-    protocol   = "tcp"
-    cidr_block = ["0.0.0.0/0"]
-  }
-
 }
 
-
-resource "aws_security_group" "internal_lb_sg" {
-  vpc_id = var.vpc_id
-
-  
-}
